25f3fcad8412c51b86d182e55030ef29401fa3ce
Hannah Silver <hannah_silver@hotmail.com>
2020-06-25 13:37:12 -0400

    exercise 2.1

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,19 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+  constructor(title, genre, author, isRead) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isRead = isRead || false;
+  }
+}
+
+const book1 = new Book("The Woman in the Dunes", "Fiction", "Kobe Abe", true);
+const book2 = new Book();
+const book3 = new Book();
+const book4 = new Book();
+const book5 = new Book();
 
 console.log(book1, book2, book3, book4, book5);
